81e1885b64115a8aa9a99f1b5a67a155fbb6094c
Bharathdevops123 <48836839+Bharathdevops123@users.noreply.github.com>
2025-06-17 21:19:23 +0530

    Update firstfile.tf

--- firstfile.tf
+++ firstfile.tf
@@ -1,3 +1,23 @@
-Create a folder in D:\Teraform 
+Create a folder in D:\Terraform 
 Create a file Helloworld.tf
-Open power shell go to D:\Teraform and run terraform init --This will initialize the terraform
+Open power shell go to D:\Terraform and run terraform init --This will initialize the terraform
+
+In the Helloworld.tf we need to specify the below
+
+As it is Aws provider terraform will understand its an aws provider and we need to provide credetials or we can provide profile or role_arn
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
+or
+
+provider "aws" {
+  shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+  profile                  = "customprofile"
+}
+
+Add this code in helloworld.tf and run terraform plan you will see the result that aws provider log
